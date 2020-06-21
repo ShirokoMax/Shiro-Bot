e149de92e830c1da2f4c9de30d17a81c0cb1c8ea
ShirokoMax <56560768+ShirokoMax@users.noreply.github.com>
2020-06-21 18:00:27 +0300

    token upd

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api'); // подключаем node-telegram-bot-api
 
-const token = ''; // тут токен кторый мы получили от botFather
+import {token} from '/token.js'; // тут токен кторый мы получили от botFather
 
 // включаем самого обота
 const bot = new TelegramBot(token, {polling: true});
