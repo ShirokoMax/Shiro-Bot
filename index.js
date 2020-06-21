d64ae00599518c8af9f7ceb333c01d8ffd7690ea
ShirokoMax <56560768+ShirokoMax@users.noreply.github.com>
2020-06-21 17:56:03 +0300

    minor

--- index.js
+++ index.js
@@ -5,68 +5,25 @@ const token = ''; // тут токен кторый мы получили от b
 // включаем самого обота
 const bot = new TelegramBot(token, {polling: true});
 
-//конфиг клавиатуры
-/*
-const keyboard = [
-    [
-      {
-        text: 'Хочу кота', // текст на кнопке
-        callback_data: 'moreKeks' // данные для обработчика событий
-      }
-    ],
-    [
-        {
-          text: 'Хочу песика',
-          callback_data: 'morePes'
-        }
-    ],
-    [
-        {
-          text: 'Хочу проходить курсы',
-          url: 'https://htmlacademy.ru/continue' //внешняя ссылка
-        }
-      ]
-  ];
-*/
-// обработчик события присылания нам любого сообщения
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id; //получаем идентификатор диалога, чтобы отвечать именно тому пользователю, который нам что-то прислал
 
-  // отправляем сообщение
-  bot.sendMessage(chatId, 'Привет, Друг! чего хочешь?', { // прикрутим клаву
-        reply_markup: {
-            inline_keyboard: keyboard
-        }
-    });
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+ 
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+ 
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
 });
-/*
-// обработчик событий нажатий на клавиатуру
-bot.on('callback_query', (query) => {
-    const chatId = query.message.chat.id;
-
-    let img = '';
-
-    if (query.data === 'moreKeks') { // если кот
-        img = 'keks.png';
-    }
-
-    if (query.data === 'morePes') { // если пёс
-        img = 'pes.png';
-    }
-
-    if (img) {
-        bot.sendPhoto(chatId, img, { // прикрутим клаву
-            reply_markup: {
-                inline_keyboard: keyboard
-            }
-        });
-    } else {
-        bot.sendMessage(chatId, 'Непонятно, давай попробуем ещё раз?', { // прикрутим клаву
-            reply_markup: {
-                inline_keyboard: keyboard
-            }
-        });
-    }
-  });
-
-*/
+ 
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+ 
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message! ( OH YEEEAH )');
+});
